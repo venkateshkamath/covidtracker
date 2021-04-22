da1b0075e1963f55b90f936f8060290424bce7de
venkateshkamath <48375542+venkateshkamath@users.noreply.github.com>
2021-04-22 19:32:41 +0530

    Add files via upload

---
+++ build/precache-manifest.613e4d5ca3be15b0149894588434ec90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b99ef858689e04c63b70ed60390dca09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed5874af5ddd9d27c8fd",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "41a5244c4fb18c3d9a10",
+    "url": "/static/css/main.02729728.chunk.css"
+  },
+  {
+    "revision": "ed5874af5ddd9d27c8fd",
+    "url": "/static/js/2.ad8e3933.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/static/js/2.ad8e3933.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41a5244c4fb18c3d9a10",
+    "url": "/static/js/main.ccd0fc6b.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
